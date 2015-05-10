a80ba7fcf81e856243cce4bc40b81706c0787931
Lyrositor <gagne.marc@gmail.com>
2015-05-10 12:28:19 +0200

    Fix variable names,

--- src/main/java/objects/Bubble.java
+++ src/main/java/objects/Bubble.java
@@ -24,31 +24,27 @@ public class Bubble extends PhysicsObject {
         return Math.PI * Math.pow(getRayon(), 2) * DENSITY;
     }
 
-    public Vector2d intersects(Bubble bubble) {
-        BoundingBox bounds1 = bounds;
-        BoundingBox bounds2 = bubble.getBounds();
-        Vector2d pos1 = position;
-        Vector2d pos2 = bubble.getPosition();
-        Vector2d diff = pos2.sub(pos1);
+    public Vector2d intersects(Bubble otherBubble) {
+        BoundingBox otherBubbleBounds = otherBubble.getBounds();
+        Vector2d otherBubblePosition = otherBubble.getPosition();
+        Vector2d diff = otherBubblePosition.sub(position);
 
-        if (diff.norm() <= bounds1.radius() + bounds2.radius())
+        if (diff.norm() <= bounds.radius() + otherBubbleBounds.radius())
             return diff.setNorm(getRayon());
         return null;
     }
 
     public Vector2d intersects(Wall wall) {
-        BoundingBox bounds1 = bounds;
-        BoundingBox bounds2 = wall.getBounds();
-        Vector2d pos1 = position;
-        Vector2d pos2 = wall.getPosition();
-        Vector2d diff = pos2.sub(pos1);
+        BoundingBox wallBounds = wall.getBounds();
+        Vector2d wallPos = wall.getPosition();
+        Vector2d diff = wallPos.sub(position);
 
-        if (bounds2.orientation() == BoundingBox.HORIZONTAL) {
-            if (Math.abs(pos2.y - pos1.y) <= bounds2.width() + bounds1.radius())
-                return new Vector2d(0, diff.y / Math.abs(diff.y) * bounds1.radius());
-        } else if (bounds2.orientation() == BoundingBox.VERTICAL) {
-            if (Math.abs(pos2.x - pos1.x) <= bounds2.width() + bounds1.radius())
-                return new Vector2d(diff.x / Math.abs(diff.x) * bounds1.radius(), 0);
+        if (wallBounds.orientation() == BoundingBox.HORIZONTAL) {
+            if (Math.abs(wallPos.y - position.y) <= wallBounds.width() + bounds.radius())
+                return new Vector2d(0, diff.y / Math.abs(diff.y) * bounds.radius());
+        } else if (wallBounds.orientation() == BoundingBox.VERTICAL) {
+            if (Math.abs(wallPos.x - position.x) <= wallBounds.width() + bounds.radius())
+                return new Vector2d(diff.x / Math.abs(diff.x) * bounds.radius(), 0);
         }
 
         return null;
